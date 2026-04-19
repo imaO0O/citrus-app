e84349a36b2ce20a3e90d4b609c935eb4f676416
Marina <sahsa.shishkov@yandex.ru>
2026-04-19 23:49:27 +0300

    fix: overflow в дыхательных упражнениях
    - exercises_screen.dart: заменил Column на SingleChildScrollView
      в секции breathing exercise (убраны Spacer, добавлены отступы)

--- apps/flutter_app/lib/core/config/api_config.dart
+++ apps/flutter_app/lib/core/config/api_config.dart
@@ -6,7 +6,7 @@
 ///   .\scripts\set-ip.ps1 -Ip 192.168.0.100 - конкретный IP
 class ApiConfig {
   /// Базовый URL backend.
-  static const String baseUrl = 'http://192.168.0.100:8081';
+  static const String baseUrl = 'http://192.168.0.101:8081';
 
   /// Freesound API
   /// Получить API ключ: https://freesound.org/help/developer/
