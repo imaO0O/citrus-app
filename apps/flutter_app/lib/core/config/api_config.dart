c71531901bfd507d697fdaeced4b5e6e5f91ed77
MarsikCat <sahsa.shishkov@yandex.ru>
2026-04-25 22:01:34 +0300

    chore: обновлён baseUrl бэкенда на локальный IP

--- apps/flutter_app/lib/core/config/api_config.dart
+++ apps/flutter_app/lib/core/config/api_config.dart
@@ -6,7 +6,7 @@
 ///   .\scripts\set-ip.ps1 -Ip 192.168.0.100 - конкретный IP
 class ApiConfig {
   /// Базовый URL backend.
-  static const String baseUrl = 'http://10.0.2.2:8081';
+  static const String baseUrl = 'http://192.168.0.110:8081';
 
   /// Freesound API
   /// Получить API ключ: https://freesound.org/help/developer/
